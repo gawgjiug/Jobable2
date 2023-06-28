fdacf8bd0c8fea1c142e7ccb7189e8a7aab7ebee
곽지욱 <kwjo0228@naver.com>
2023-06-28 23:13:02 +0900

    2023 06 28

--- app/src/main/java/com/example/capstone/UserBoardActivity.kt
+++ app/src/main/java/com/example/capstone/UserBoardActivity.kt
@@ -4,11 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageView
+import androidx.appcompat.widget.SearchView
 import com.example.capstone.board.BoardInsideActivity
 import com.example.capstone.board.BoardListLVAdapter
 import com.example.capstone.board.BoardModel
-import com.example.capstone.databinding.ActivityCeoIntroBinding
 import com.example.capstone.databinding.ActivityUserBoardBinding
 import com.example.capstone.setting.SettingActivity
 import com.example.capstone.utils.FBRef
@@ -18,6 +17,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class UserBoardActivity : AppCompatActivity() {
 
@@ -27,21 +27,29 @@ class UserBoardActivity : AppCompatActivity() {
     private val boardDataList = mutableListOf<BoardModel>()
     private lateinit var boardRVAdapter: BoardListLVAdapter
     private val boardKeyList = mutableListOf<String>()
+    private lateinit var searchView: SearchView
+    private val filteredBoardDataList = mutableListOf<BoardModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         binding = ActivityUserBoardBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        boardRVAdapter = BoardListLVAdapter(boardDataList)
+        boardRVAdapter = BoardListLVAdapter(filteredBoardDataList)
         binding.userboardListView.adapter = boardRVAdapter
-        binding.userboardListView.setOnItemClickListener { parent, view, position, id ->
-            val intent = Intent(this,BoardInsideActivity::class.java)
-            intent.putExtra("key",boardKeyList[position])
-            startActivity(intent)
-        }
+
+        searchView = findViewById(R.id.serachView)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterBoardData(newText)
+                return true
+            }
+        })
+
         auth = Firebase.auth
 
         val usersetting_btn = binding.UsersettingBtn
@@ -50,42 +58,54 @@ class UserBoardActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-
+        binding.userboardListView.setOnItemClickListener { parent, view, position, id ->
+            val intent = Intent(this, BoardInsideActivity::class.java)
+            intent.putExtra("key", boardKeyList[position])
+            startActivity(intent)
+        }
 
         getFBBoardData()
-
     }
 
-    private fun getFBBoardData(){
+    private fun getFBBoardData() {
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
+                boardDataList.clear()
 
-                boardDataList.clear() //기존 데이터 초기화 하고 다시 받아옴
-
-                for (dataModel in dataSnapshot.children){
-                    Log.d(TAG,dataModel.toString())
-//                    dataModel.key
+                for (dataModel in dataSnapshot.children) {
                     val item = dataModel.getValue(BoardModel::class.java)
                     boardDataList.add(item!!)
                     boardKeyList.add(dataModel.key.toString())
-
                 }
-                boardKeyList.reverse()
-                boardDataList.reverse() //글을 최신순으로 보여주기 위해 list를 뒤집어줌
-                boardRVAdapter.notifyDataSetChanged()
-                Log.d(TAG,boardDataList.toString())
 
+                boardKeyList.reverse()
+                boardDataList.reverse()
+                filterBoardData(searchView.query.toString())
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                Log.w(TAG,"loadPost:onCancellde",databaseError.toException())
-
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
             }
-
         }
+
         FBRef.boardRef.addValueEventListener(postListener)
     }
 
+    private fun filterBoardData(query: String?) {
+        filteredBoardDataList.clear()
 
+        if (query.isNullOrEmpty()) {
+            filteredBoardDataList.addAll(boardDataList)
+        } else {
+            val searchQuery = query.toLowerCase(Locale.getDefault())
 
+            for (item in boardDataList) {
+                if (item.title.toLowerCase(Locale.getDefault()).contains(searchQuery)) {
+                    filteredBoardDataList.add(item)
+                }
+            }
+        }
+
+        boardRVAdapter.notifyDataSetChanged()
+    }
 }
