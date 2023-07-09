f5b3f22df2494c90d808a506be8d6b4855bbba70
곽지욱 <kwjo0228@naver.com>
2023-07-09 21:49:49 +0900

    2023 07 09 webview 완료

--- app/src/main/java/com/example/capstone/WelfareActivity.kt
+++ app/src/main/java/com/example/capstone/WelfareActivity.kt
@@ -3,14 +3,13 @@ package com.example.capstone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.ImageView
 import com.example.capstone.databinding.ActivityWelfareBinding
-import com.example.capstone.welfarewebview.WelfareSeoulActivity
 
 class WelfareActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityWelfareBinding
+    private lateinit var binding: ActivityWelfareBinding
     private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,15 +17,23 @@ class WelfareActivity : AppCompatActivity() {
 
         binding = ActivityWelfareBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val welFareSuwonImageView = findViewById<ImageView>(R.id.WelfareSuwon)
 
-        binding.WelfareSeoul.setOnClickListener {
-            val intent = Intent(this,WelfareSeoulActivity::class.java)
-            startActivity(intent)
-        }
+        val welfareMap = mapOf(
+            binding.WelfareSuwon to "https://www.suwonrehab.or.kr/bbs/content.php?co_id=bussiness5",
+            binding.WelfareSeoul to "https://wis.seoul.go.kr/handicap/findWelfareService.do",
+            binding.WelfareSiheung to "https://www.siheung.go.kr/portal/contents.do?mId=0204070000",
+            binding.WelfareAnsan to "https://www.ansan.go.kr/www/common/cntnts/selectContents.do?cntnts_id=C0001245",
+            binding.WelfareDaegu to "https://www.daegu.go.kr/welf/index.do?menu_id=00000503&servletPath=%2Fwelf",
+            binding.WelfareWonju to "https://www.wonju.go.kr/welfare/contents.do?key=2252&",
+            binding.WelfareGwangju to "https://gjdsc.or.kr/kor/menu?menuId=71_110",
+            binding.WelfareIncheon to "https://www.incheon.go.kr/welfare/WE010201",
+            binding.elseIcon to "https://www.ablejob.co.kr/eduEvent/edu-event.php"
+        )
 
-        welFareSuwonImageView.setOnClickListener {
-            showWebView("https://www.suwonrehab.or.kr/bbs/content.php?co_id=bussiness5")
+        welfareMap.forEach { (imageView, url) ->
+            imageView.setOnClickListener {
+                showWebView(url)
+            }
         }
     }
 
@@ -42,7 +49,7 @@ class WelfareActivity : AppCompatActivity() {
         if (webView.canGoBack()) {
             webView.goBack()
         } else {
-            super.onBackPressed()
+            finish()
         }
     }
 }
