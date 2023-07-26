ef82739112929725990a21c9acd5a16cded921b7
곽지욱 <kwjo0228@naver.com>
2023-07-26 23:24:15 +0900

    2023 07 26

--- app/src/main/java/com/example/capstone/ResumecheckActivity.kt
+++ app/src/main/java/com/example/capstone/ResumecheckActivity.kt
@@ -1,11 +1,90 @@
 package com.example.capstone
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.capstone.databinding.ActivityResumcheckBinding
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class ResumecheckActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResumcheckBinding
+    private val usersList = mutableListOf<User>()
+    private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_resumcheck)
+        binding = ActivityResumcheckBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        database = FirebaseDatabase.getInstance().reference
+        auth = FirebaseAuth.getInstance()
+
+        // Get the selected user's ID from the previous activity
+        val selectedUserId = intent.getStringExtra("selectedUserId")
+
+        if (selectedUserId != null) {
+            // Get the selected user's information from the database
+            val userListener = object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val user = dataSnapshot.getValue(User::class.java)
+                    if (user != null) {
+                        // Add the selected user's information to the list
+                        usersList.add(user)
+                        // Update the ListView with user information
+                        updateListView()
+                        // Show the selected user's profile image, name, and age
+                        showProfileInfo(user)
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Handle error
+                }
+            }
+
+            // Get the reference to the selected user's information in the database
+            val userRef = database.child("users").child(selectedUserId)
+            // Register the listener to get the selected user's information
+            userRef.addListenerForSingleValueEvent(userListener)
+        }
+
+        // Set an onClickListener for the ListView items
+        binding.listViewUsers.setOnItemClickListener { _, _, position, _ ->
+            // Get the selected user's information
+            val selectedUser = usersList[position]
+            // Start the DetailedResumeActivity and pass the selected user's ID
+            val intent = Intent(this, DetailedResumeActivity::class.java)
+            intent.putExtra("selectedUserId", selectedUser.userId)
+            startActivity(intent)
+        }
+    }
+
+    private fun updateListView() {
+        // Update the ListView with the user information
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, usersList)
+        binding.listViewUsers.adapter = adapter
+    }
+
+    private fun showProfileInfo(user: User) {
+        // Show the selected user's profile image, name, and age
+        Glide.with(this)
+            .load(user.profileImageUrl)
+            .into(binding.imageViewProfile)
+
+        binding.textViewName.text = "이름: ${user.name}"
+        binding.textViewAge.text = "나이: ${user.age}"
+    }
+
+    // User data class
+    data class User(val userId: String, val name: String, val age: Int, val profileImageUrl: String) {
+        override fun toString(): String {
+            return name
+        }
     }
 }
--- app/src/main/java/com/example/capstone/board/BoardInsideActivity.kt
+++ app/src/main/java/com/example/capstone/board/BoardInsideActivity.kt
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
@@ -14,7 +12,9 @@ import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.example.capstone.R
 import com.example.capstone.RegisterActivity
+import com.example.capstone.ResumecheckActivity
 import com.example.capstone.databinding.ActivityBoardInsideBinding
+import com.example.capstone.databinding.CustomDialogBinding
 import com.example.capstone.utils.FBAuth
 import com.example.capstone.utils.FBRef
 import com.google.android.gms.tasks.OnCompleteListener
@@ -36,125 +36,131 @@ class BoardInsideActivity : AppCompatActivity() {
     private lateinit var database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-
-
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_board_inside)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_board_inside)
         binding.boardSettingicon.setOnClickListener {
             showDialog()
         }
 
-
-
-
-
-
         key = intent.getStringExtra("key").toString()
-
         database = FirebaseDatabase.getInstance().reference
 
         getBoardData(key)
         getImageData(key)
+        // Check if the current user has a resume and show/hide the apply job button accordingly
+        checkUserHasResume()
 
-
-
+        // Set an onClickListener to apply job button
+        binding.applyjobBtn.setOnClickListener {
+            // Get the selected user's data from the database and pass it to ResumecheckActivity
+            val selectedUserId = key // This key is used as the user's unique ID
+            val intent = Intent(this, ResumecheckActivity::class.java)
+            intent.putExtra("selectedUserId", selectedUserId)
+            startActivity(intent)
+        }
     }
-    private fun showDialog(){
-        val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog,null)
+
+    private fun showDialog() {
+        // Create a custom dialog using custom_dialog.xml
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
+        val dialogBinding = CustomDialogBinding.bind(mDialogView)
+
         val mBuilder = AlertDialog.Builder(this)
             .setView(mDialogView)
             .setTitle("게시글 수정/삭제")
 
         val alertDialog = mBuilder.show()
-        alertDialog.findViewById<Button>(R.id.editBtn)?.setOnClickListener {
-            Toast.makeText(this,"수정 버튼을 눌렀습니다",Toast.LENGTH_LONG).show()
-            val intent = Intent(this,BoardEditActivity::class.java)
-            intent.putExtra("key",key) //edit으로 intent를 넘겨줄 때 key값을 같이 넘겨줄수 있도록 함
-            //수정 페이지이기 때문에 수정버튼 을 눌렀을때 key값을 바탕으로 기존의 제목과 내용을 포함하고 있어야 함.
+
+        dialogBinding.editBtn.setOnClickListener {
+            alertDialog.dismiss()
+            Toast.makeText(this, "수정 버튼을 눌렀습니다", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, BoardEditActivity::class.java)
+            intent.putExtra("key", key)
             startActivity(intent)
         }
-        alertDialog.findViewById<Button>(R.id.removeBtn)?.setOnClickListener {
 
-            FBRef.boardRef.child(key).removeValue() //board테이블의 대한 정보를 가져와서 key값을 찾아 remove해줌
-            Toast.makeText(this,"삭제완료",Toast.LENGTH_SHORT).show()
-            finish() //삭제하고 activity가 닫히도록 finish선언
+        dialogBinding.removeBtn.setOnClickListener {
+            alertDialog.dismiss()
+            FBRef.boardRef.child(key).removeValue()
+            Toast.makeText(this, "삭제완료", Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
-    private fun getBoardData(key : String){
+
+    private fun getBoardData(key: String) {
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-
-                try{
+                try {
                     val dataModel = dataSnapshot.getValue(BoardModel::class.java)
                     val userdataModel = dataSnapshot.getValue(RegisterActivity.User::class.java)
-                    Log.d(TAG,dataModel!!.title)
+                    Log.d(TAG, dataModel!!.title)
 
                     binding.titleArea.text = dataModel!!.title
                     binding.textAread.text = dataModel!!.content
                     binding.timeArea.text = dataModel!!.time
 
+                    val myUid = FBAuth.getUid()
+                    val writerUid = dataModel.uid
 
-
-
-                    val myUid = FBAuth.getUid() //로그인 한 uid
-                    val writerUid = dataModel.uid //글쓴사용자의 uid
-
-
-                    val userQuery : Query = database.child("users").orderByChild("uid").equalTo(myUid)
-
-                    if (myUid.equals(writerUid)){
+                    val userQuery: Query = database.child("users").orderByChild("uid").equalTo(myUid)
+                    if (myUid.equals(writerUid)) {
                         binding.boardSettingicon.isVisible = true
-                    }else{
-
+                    } else {
+                        // Handle the case where the current user is not the author of the post
                     }
-                    checkUserExists(myUid)
 
+                    // Try to check if the user has a resume and show/hide the apply job button
+                    checkUserHasResume()
 
-
-                    //try에서 에러가 나면 catch를 실행하라 예외처리
-
-                }catch (e:java.lang.Exception){
-                    Log.d(TAG,"삭제완료")
+                } catch (e: java.lang.Exception) {
+                    Log.d(TAG, "삭제완료")
                 }
-
-
-
-
             }
-
 
             override fun onCancelled(databaseError: DatabaseError) {
-                Log.w(TAG,"loadPost:onCancellde",databaseError.toException())
-
+                Log.w(TAG, "loadPost:onCancellde", databaseError.toException())
             }
-
         }
         FBRef.boardRef.child(key).addValueEventListener(postListener)
     }
 
-    private fun checkUserExists(uid: String) {
-        val userQuery: Query = database.child("users").orderByChild("uid").equalTo(uid)
-        userQuery.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    // 사용자가 존재하는 경우
-                    binding.applyjobBtn.isVisible = true
-                } else {
-                    // 사용자가 존재하지 않는 경우
-                    binding.applyjobBtn.isVisible = false
-                }
-            }
+    private fun checkUserHasResume() {
+        val myUid = FBAuth.getUid()
+        if (myUid != null) {
+            val resumeReference = FirebaseDatabase.getInstance().reference.child("resume").child(myUid)
+            resumeReference.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        // If the user has a resume, show the apply job button
+                        binding.applyjobBtn.isVisible = true
+                        // Set an onClickListener to apply job button
+                        binding.applyjobBtn.setOnClickListener {
+                            // Get the selected user's data from the database and pass it to ResumecheckActivity
+                            val selectedUserId = key // This key is used as the user's unique ID
 
-            override fun onCancelled(databaseError: DatabaseError) {
-                Log.w(TAG, "checkUserExists:onCancelled", databaseError.toException())
-            }
-        })
+                            intent.putExtra("selectedUserId", selectedUserId)
+
+                        }
+                    } else {
+                        // If the user does not have a resume, hide the apply job button
+                        binding.applyjobBtn.isVisible = false
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Log.w(TAG, "checkUserHasResume:onCancelled", databaseError.toException())
+                }
+            })
+        } else {
+            // Handle the case where the user is not logged in
+            // You may want to hide the apply job button or redirect the user to login page, etc.
+            binding.applyjobBtn.isVisible = false
+        }
     }
 
     private fun getImageData(key :String){
         // Reference to an image file in Cloud Storage
-        val storageReference = Firebase.storage.reference.child(key + ".jpg")
+        val storageReference = Firebase.storage.reference.child("$key.jpg")
 
         // ImageView in your Activity
         val imageViewFromFB = binding.getImageArea
@@ -165,7 +171,7 @@ class BoardInsideActivity : AppCompatActivity() {
                     .load(task.result)
                     .into(imageViewFromFB)
             }else{
-
+                // Handle the failure to load the image
             }
         })
     }
